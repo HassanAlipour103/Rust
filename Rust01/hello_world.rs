e0fe15ee12105aea987a4197e9c177a032f87cd1
Hassan Alipour <hassan.a103@gmail.com>
2024-03-20 01:04:44 +0330

    hello_world

--- Rust01/hello_world.rs
+++ Rust01/hello_world.rs
@@ -1,3 +1,14 @@
 fn main(){
     println!("hello rust...");
 }
+
+/*
+    1. create hello_world.rs file
+    2. fn main() this is th main function and start point of our program
+    3. println!() is a macro which we will learn later
+    4. dont forget the ; at the end of each line
+    5. save the file and run "rustc hello_world.rs"
+        5.1. this will create an executable file named "hello_world"
+    6. at last we run the created executable file "./hello_world"
+
+*/
